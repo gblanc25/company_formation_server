ac91b61a73ca12543cb16b58fec4413b34237402
gblanc25 <gleblanc@college.harvard.edu>
2025-03-12 12:07:52 -0400

    only works with delaware entities, so removed all other states from selection

--- app.py
+++ app.py
@@ -174,12 +174,7 @@ def form_company_schema():
 @app.route('/', methods=['GET'])
 def company_form():
     states = [
-        'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-        'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-        'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-        'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-        'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY',
-        'DC', 'PR', 'GU', 'VI', 'AS', 'MP'
+        'DE'
     ]
     
     return f'''
